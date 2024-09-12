41e4f2641916a4729275cc3b7923d3e823e6ad34
Santiago Rodriguez <santiara010@gmail.com>
2024-09-11 21:30:16 -0300

    Le agregue una imagen y incorpore bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 
 import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Formulario from './components/Formulario'
 
 function App() {
 
   return (
-   <main>
+   <main className=''>
+    <div className='d-flex justify-content-center mt-4'>
+    <img className='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQzC6pi-QUjeZ_z4ukUXrtLc06zuUH1V400A&s' alt="Logo" />
+    </div>
+    <h1 className='text-center mt-3'><b>Cat list</b></h1>
     <Formulario></Formulario>
    </main>
   )
